fb4539206796985cfe778f78d3217ad5e0607608
Denny Wu <email": null>
2016-08-08 15:03:39 -0700

    Attempt checkpoint challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -10,9 +10,16 @@ class SodaMachine
   end
 
   def find_soda(soda_brand)
+    if sodas == true
+      return soda_machine.find_soda(soda_brand)
+    else
+      return nil
+    end
   end
 
   def sell(soda_brand)
   end
 
 end
+
+soda_machine = SodaMachine.new(sodas = {brand: 'Pepsi', price: 0.65})
