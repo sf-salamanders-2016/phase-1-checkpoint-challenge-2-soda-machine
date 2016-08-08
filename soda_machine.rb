e618c330d68a09d349dd233add7683fa420b8fd0
cvilla100@gmail.com <cvilla100@gmail.com>
2016-08-08 15:04:25 -0700

    Attempted #sell

--- soda_machine.rb
+++ soda_machine.rb
@@ -22,6 +22,16 @@ class SodaMachine
   end
 
   def sell(soda_brand)
+    if find_soda(soda_brand) != nil
+      #find soda price
+      current_soda = self.sodas.select {|soda| soda.brand == soda_brand}
+
+      #add to cash
+      self.cash += current_soda.price
+
+      #remove from machine
+      self.sodas.delete(current_soda)
+    end
   end
 
 end
@@ -33,4 +43,3 @@ second_pepsi = Soda.new(brand: 'Pepsi', price: 0.65)
 
 mac = SodaMachine.new(sodas: [pepsi, mountain_dew, coke_zero, second_pepsi], cash: 1.00)
 
-# Hash --> array
