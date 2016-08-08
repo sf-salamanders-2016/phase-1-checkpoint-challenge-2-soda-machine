db8acae5b6be1d08fb219d7c55a392158bc4dcbb
Blake  A Wills <email": null>
2016-08-08 14:39:02 -0700

    Update soda_machine methods

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,4 @@
+require 'soda'
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +8,27 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @soda.include?(soda_brand) ? soda_brand : nil
   end
 
   def sell(soda_brand)
+    @sodas = @sodas - [soda_brand]
+    add_price
+
   end
+#subtract brand name from soda machine
+#add price to soda machine cash
+
+  private
+
+  def add_price(soda_brand)
+    @cash = @soda[soda_brand].price + @cash
+    @cash
+  end
+
 
 end
