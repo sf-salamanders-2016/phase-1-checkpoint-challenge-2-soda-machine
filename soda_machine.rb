6d69279434673799e7b1c8755237107d84cf927f
Kayden Williams <email": null>
2016-08-08 15:18:46 -0700

    Add methods and pass tests

--- soda_machine.rb
+++ soda_machine.rb
@@ -11,9 +11,28 @@ class SodaMachine
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |item|
+      if item.brand == soda_brand
+        return item
+      else
+        return nil
+      end
+    end
   end
 
   def sell(soda_brand)
+    @sodas.each do |item|
+      if item.brand == soda_brand
+        @cash += item.price
+        return @sodas.delete(item)
+      end
+    end
+    return nil
   end
 
 end
+
+
+
+
+
