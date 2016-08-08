c1ae1d720168593687e2b1d6e920edf5bf10ec3d
cvilla100@gmail.com <cvilla100@gmail.com>
2016-08-08 14:44:20 -0700

    Add #find_soda

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative 'soda'
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -9,10 +11,26 @@ class SodaMachine
   def current_inventory_count
   end
 
-  def find_soda(soda_brand)
+  def find_soda(soda_brand) 
+    self.sodas.each do |soda| 
+      if soda.brand == soda_brand
+        return soda
+      else
+        return nil
+      end
+    end
   end
 
   def sell(soda_brand)
   end
 
 end
+
+pepsi = Soda.new(brand: 'Pepsi', price: 0.65)
+mountain_dew = Soda.new(brand: 'Mountain Dew', price: 0.75)
+coke_zero = Soda.new(brand: 'Coke Zero', price: 1.00)
+second_pepsi = Soda.new(brand: 'Pepsi', price: 0.65)
+
+mac = SodaMachine.new(sodas: [pepsi, mountain_dew, coke_zero, second_pepsi], cash: 1.00)
+
+# Hash --> array
