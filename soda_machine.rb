a4da5e880e9c9e1a917fd6c6b226d01fd4ba8927
Long Tran <artsandcrass@gmail.com>
2016-08-08 15:17:57 -0700

    added code to soda machines

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,9 +7,13 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    if @sodas.brand.inlcude?(soda_brand)
+      return soda_brand
+    end
   end
 
   def sell(soda_brand)
